ba9662dbe1a072596bf829c87ec4256433899f76
ChegueEPDS <kovacs@epds.hu>
2026-07-03 22:14:42 +0200

    customdom

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://epds-admin-drhwggcdh8aneffd.westeurope-01.azurewebsites.net',
+  apiUrl: 'https://opsapi.epds.hu',
   microsoftClientId: '5e20ba3e-a873-4774-bc2b-2b69f158fc7a',
   microsoftTenantId: 'c7b7e4b5-d197-4a58-b592-4471870b8556'
 };
